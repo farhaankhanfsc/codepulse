26dda39e701fdc7c845a7590ba94c8a82571c826
Farhaan Khan <Farhaan.Khan@fsc.com.fj>
2024-01-10 09:47:44 +1200

    commit push

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
+  private addCategorySubscribtion?: Subscription;
 
-  constructor()
+  constructor(private categoryService: CategoryService)
   {
     this.model = {
       name: '',
       urlHandle: ''
     };
   }
+  
 
   onFormSubmit()
   {
-    console.log(this.model);
+    this.addCategorySubscribtion = this.categoryService.addCategory(this.model)
+    .subscribe({
+      next: (response)=>{
+        console.log('This was successfull');
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscribtion?.unsubscribe();
   }
 }
